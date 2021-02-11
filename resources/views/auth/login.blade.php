eddd2d84058f90a0c5f67dddca028c588bfb19e4
muamarsahruj <72781867+muamarsahruj@users.noreply.github.com>
2021-02-11 18:26:01 +0700

    Push Pertama
    membuat tampilan login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login Page</title>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container vh-100">
+		<div class="row justify-content-center h-100">
+			<div class="card w-25 my-auto shadow">
+				<div class="card-header text-center bg-primary text-white">
+					<h2>Login form</h2>
+				</div>
+				<div class="card-body">
+					<form action="" method="">
+						<div class="form-group">
+							<label for="email">Email</label>
+							<input type="email" id="email" class="form-control" name="" />
+						</div>
+						<div class="form-group">
+							<label for="password">Password</label>
+							<input type="password" id="password" class="form-control" name="" />
+						</div>
+						<input type="submit" class="btn btn-primary w-100" value="Login" name="">
+					</form>
+				</div>
+				<div class="card-footer text-right">
+					<small>&copy; Technical Babaji</small>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
